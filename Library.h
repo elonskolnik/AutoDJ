33ff3b70e26aa1522a6f810fdcf69c6ce4ef4873
elonskolnik <42852166+elonskolnik@users.noreply.github.com>
2018-11-27 11:46:09 -0500

    Add files via upload

---
+++ Library.h
@@ -0,0 +1,17 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include "Song.h"
+#include "Playlist.h"
+#include <iostream>
+
+class Library {
+private:
+    ArrayList songList;
+    ArrayList playLists;
+
+public:
+
+};
+
+#endif //LIBRARY_H
