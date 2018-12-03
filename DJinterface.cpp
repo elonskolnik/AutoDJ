ff1dbc7b20b13dcf673ebfb52d15dc2452999ec9
Ryan King <r_king7@icloud.com>
2018-12-03 17:59:42 -0500

    commit test

--- DJinterface.cpp
+++ DJinterface.cpp
@@ -7,5 +7,24 @@
 int main(){
     std::cout<<"Hello Ryan" <<std::endl;
 
+    bool program = true;
+    std::string command;
+
+    while(program){
+        std::cout<<"Enter a command or enter 'help' for assistance" <<std::endl;
+        std::getline(std::cin, command);
+
+        if(command==""){
+
+        }
+
+
+
+        else{
+            std::cout<<"Error. System could not process your command." <<std::endl;
+        }
+    }
+
+
     return 0;
 }
