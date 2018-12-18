c056787b84ad52db4c8021a15b71ad0b720e89ca
Ryan King <r_king7@icloud.com>
2018-12-18 00:03:16 -0500

    stuff changed

---
+++ Tester.cpp
@@ -0,0 +1,14 @@
+//  main.cpp
+//  TreeProject
+
+#include <iostream>
+#include "Library.h"
+#include "Playlist.h"
+#include "Song.h"
+#include "SongList.h"
+#include "SongLinked.h"
+#include "TestLib.h"
+main(){
+
+    return 0;
+}
